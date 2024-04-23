71973e3ece0303d4b9994b57c3adb300cf12e9e9
AnnyaB <anabordin2008@gmail.com>
2024-04-23 14:26:44 -0300

    melhoria

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,18 +1,16 @@
 ﻿// 3. faça um programa que leia 5 números em um vetor e ordene-os no segundo vetor;
 //bubble sort
 
-int tamanho = 5;
-int[] vetorOriginal = new int[tamanho];
-int[] vetorOrdenado = new int[tamanho];
-int aux;
+int tamanho = 5, aux;
+int[] vetorOriginal = new int[tamanho], vetorOrdenado = new int[tamanho];
 
 Console.WriteLine("\nVetor sem ordenação:");
 
-for (int i = 0; i < 5; i++)
+for (int i = 0; i < tamanho; i++)
 {
     vetorOriginal[i] = new Random().Next(0, 100);
     vetorOrdenado[i] = vetorOriginal[i];    
-    Console.Write($"{vetorOrdenado[i]}, ");
+    Console.Write($"{vetorOriginal[i]}, ");
 }
 
 for (int referencia = 0; referencia < tamanho - 1; referencia++)
@@ -29,7 +27,6 @@ for (int referencia = 0; referencia < tamanho - 1; referencia++)
 }
 Console.WriteLine("\n\nVetor com ordenação:");
 for (int i = 0; i < tamanho; i++)
-{
     Console.Write($"{vetorOrdenado[i]}, ");
-}
+
 Console.WriteLine("\n");
