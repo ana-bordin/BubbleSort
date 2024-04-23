4472d12973a703ae556dd5c2cbe2c352e761fa96
AnnyaB <anabordin2008@gmail.com>
2024-04-23 14:57:00 -0300

    vetor sem repetidos

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,16 +1,20 @@
-﻿// 3. faça um programa que leia 5 números em um vetor e ordene-os no segundo vetor;
-//bubble sort
+﻿//faça um programa em que o vetor 2 tenha numeros ordenados e sem repetidos;
 
 int tamanho = 5, aux;
 int[] vetorOriginal = new int[tamanho], vetorOrdenado = new int[tamanho];
+bool zero = false;
 
 Console.WriteLine("\nVetor sem ordenação:");
 
 for (int i = 0; i < tamanho; i++)
 {
-    vetorOriginal[i] = new Random().Next(0, 100);
+    vetorOriginal[i] = new Random().Next(0, 5);
     vetorOrdenado[i] = vetorOriginal[i];    
     Console.Write($"{vetorOriginal[i]}, ");
+    if (vetorOrdenado[i] == 0)
+    {
+        zero = true;
+    }
 }
 
 for (int referencia = 0; referencia < tamanho - 1; referencia++)
@@ -22,11 +26,28 @@ for (int referencia = 0; referencia < tamanho - 1; referencia++)
             aux = vetorOrdenado[referencia];
             vetorOrdenado[referencia] = vetorOrdenado[comparacao];
             vetorOrdenado[comparacao] = aux;
+            
+        }
+        if (vetorOrdenado[referencia] == vetorOrdenado[comparacao])
+        {
+            vetorOrdenado[comparacao] = 0;
         }
     }
 }
+
 Console.WriteLine("\n\nVetor com ordenação:");
 for (int i = 0; i < tamanho; i++)
-    Console.Write($"{vetorOrdenado[i]}, ");
+    if (vetorOrdenado[i] == 0)
+    {
+        if (zero == true)
+        {
+            Console.Write($"{vetorOrdenado[i]}, ");
+            zero = false;
+        }
+    }
+    else
+    {
+        Console.Write($"{vetorOrdenado[i]}, ");
+    }
 
 Console.WriteLine("\n");
